7a11e7f189782bad3ab4f8dffa7f23c6bb67d572
Akhyar Amarullah <akhyrul@gmail.com>
2017-09-14 22:50:45 +0700

    Prepare setup.py for distribution

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 from setuptools import setup, find_packages
 
+GITHUB_REPO='chickenzord/dotenvy'
+VERSION='0.1.0'
+
 setup(
-    name="dotenvy",
-    version="0.1.0",
+    name='dotenvy',
+    version=VERSION,
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    install_requires=[],
+    install_requires=['future'],
     package_data={},
 
-    # metadata for upload to PyPI
-    author="Akhyar Amarullah",
-    author_email="akhyrul@gmail.com",
-    description="dotenv handler for python",
-    license="MIT",
-    keywords="dotenv",
-    url="https://projects.chickenzord.com/dotenvy",
+    # metadata
+    author='Akhyar Amarullah',
+    author_email='akhyrul@gmail.com',
+    description='dotenv handler for python',
+    long_description=open('README.rst').read(),
+    download_url='https://github.com/%s/archive/%s.tar.gz' % (GITHUB_REPO, VERSION),
+    license='MIT',
+    keywords=['dotenv', 'configuration', 'environment'],
+    url='https://github.com/%s' % (GITHUB_REPO),
 )
